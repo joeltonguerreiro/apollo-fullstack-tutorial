e63133d65ec88f1a8ad8f08b05a1031d7780fdbd
Joelton Guerreiro <joeltonguerreiro@gmail.com>
2019-06-12 00:35:50 -0300

    projeto apollo finalizado

---
+++ start/client/src/index.js
@@ -0,0 +1,50 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
+
+import { ApolloProvider, Query } from 'react-apollo';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import Login from './pages/login'
+import injectStyles from './styles';
+
+import { resolvers, typeDefs } from './resolvers';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'http://localhost:4000/'
+})
+
+const client = new ApolloClient({
+    cache,
+    link: new HttpLink({
+        uri: 'http://localhost:4000/graphql',
+        headers: { authorization: localStorage.getItem('token'), },
+    }),
+    typeDefs,
+    resolvers,
+})
+
+cache.writeData({
+    data: {
+        isLoggedIn: !!localStorage.getItem('token'),
+        cartItems: [],
+    },
+});
+
+const IS_LOGGED_IN = gql`  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+injectStyles();
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Query query={IS_LOGGED_IN}>
+            {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+        </Query>
+    </ApolloProvider>,
+    document.getElementById('root'),
+);
